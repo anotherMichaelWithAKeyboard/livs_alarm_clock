c9eb8d01b163086424ea0c81f6fbe746f6cf58df
Mikaere <mccarm110@gmail.com>
2026-04-30 16:41:16 +1000

    updated install

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 # Configuration
-PROJECT_DIR="$HOME/livs_alarm_clock"
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SERVICE_NAME="alarm-clock.service"
 USERNAME="${SUDO_USER:-$USER}"
 
